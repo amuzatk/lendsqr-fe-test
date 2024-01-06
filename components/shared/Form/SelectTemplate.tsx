0c8626e299f38fcf8f63fea028a639ce1411c3ae
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-01-06 09:36:47 +0100

    new11

--- components/shared/Form/SelectTemplate.tsx
+++ components/shared/Form/SelectTemplate.tsx
@@ -1,12 +1,23 @@
-// SimpleDropdown.js
 import React from 'react';
 
 const SelectTemplate = ({ onChange }) => {
   return (
-    <div>
+    <div style={{
+      // border:"1px solid red",
+      
+    }}>
       {/* <label htmlFor="simple-dropdown">Select an option:</label> */}
-      <select id="simple-dropdown" onChange={onChange}>
-      <option >Select Organization</option>
+      <select 
+      style={{
+        border:"none",
+        height:"35px",
+        // width:"160px",
+        color:"#213F7D",
+        fontWeight:"400",
+        lineHeight:"18.77px",
+        fontSize:"16px"
+        }} id="simple-dropdown" onChange={onChange}>
+      <option >Switch Organization</option>
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
       </select>
@@ -15,42 +26,3 @@ const SelectTemplate = ({ onChange }) => {
 };
 
 export default SelectTemplate;
-
-
-
-// import React, { useState } from 'react';
-// import { Select, Space } from 'antd';
-// // import { Select, Space } from 'antd/es';
-// // import 'antd/dist/antd.css';
-
-// const provinceData = ['Zhejiang', 'Jiangsu'];
-
-// const cityData = {
-//   Zhejiang: ['Hangzhou', 'Ningbo', 'Wenzhou'],
-//   Jiangsu: ['Nanjing', 'Suzhou', 'Zhenjiang'],
-// };
-
-// type CityName = keyof typeof cityData;
-
-// const SelectTemplate = () => {
-//   const [cities, setCities] = useState(cityData[provinceData[0] as CityName]);
-
-//   const handleProvinceChange = (value: CityName) => {
-//     setCities(cityData[value]);
-//   };
-
-//   return (
-//     <Space wrap>
-//       <Select
-//        defaultValue="Switch Organization"
-//         // defaultValue={provinceData[0]}
-//         style={{ width: "180px", fontSize:"80px", border:"none" }}
-//         onChange={handleProvinceChange}
-//         options={provinceData.map((province) => ({ label: province, value: province }))}
-//       />
-//     </Space>
-//   );
-// };
-
-// export default SelectTemplate;
-
