d34a8bb777e0b9abfc1773036d0876d4a4b389df
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-01-06 12:07:34 +0100

    new

---
+++ pages/guarantors.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const guarantors = () => {
+  return (
+    <Layout isDetailPage={false}>
+      <h2>Guarantors Page</h2>
+    </Layout>
+  )
+}
+
+export default guarantors
