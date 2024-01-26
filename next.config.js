16899437b15401a567398c4c256fc1dc476789c8
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-01-26 13:17:40 +0100

    refactored next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,43 @@
+// next.config.js
+
 module.exports = {
-    images: {
-      domains: ['i.pravatar.cc'],
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ['i.pravatar.cc'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+
+
+
+
+// module.exports = {
+//     images: {
+//       domains: ['i.pravatar.cc'],
+//     },
+//     webpack(config) {
+//       config.module.rules.push({
+//         test: /\.svg$/,
+//         use: ["@svgr/webpack"],
+//       });
   
-      return config;
-    },
-  };
+//       return config;
+//     },
+//   };
   
