48120c8e5e0bdf67e5e3be8b7127198323309270
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-01-26 12:53:51 +0100

    created kazmatics2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,39 @@
+// module.exports = {
+//     images: {
+//       domains: ['i.pravatar.cc'],
+//     },
+//     webpack(config) {
+//       config.module.rules.push({
+//         test: /\.svg$/,
+//         use: ["@svgr/webpack"],
+//       });
+  
+//       return config;
+//     },
+//   };
+  
+
+// next.config.js
+
 module.exports = {
-    images: {
-      domains: ['i.pravatar.cc'],
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-  
-      return config;
-    },
-  };
-  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ['i.pravatar.cc'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
